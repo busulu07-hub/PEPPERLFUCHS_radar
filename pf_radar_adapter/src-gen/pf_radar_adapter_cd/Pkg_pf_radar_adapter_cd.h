c7af3f82e0adfb40914c05f735137f39a43ae6b2
Busu <busulu07@gmail.com>
2025-10-23 13:42:49 +0200

    Add custom radar node and workspace updates

---
+++ pf_radar_adapter/src-gen/pf_radar_adapter_cd/Pkg_pf_radar_adapter_cd.h
@@ -0,0 +1,37 @@
+// --------------------------------------------------------
+// Copyright (c)
+//
+// contributions by author
+//                  author@somewhere.net
+// maintained by    maintainer
+//                  maintainer@somewhere.net
+
+#ifndef PKG_PF_RADAR_ADAPTER_CD
+#define PKG_PF_RADAR_ADAPTER_CD
+
+/************************************************************
+ Pkg_pf_radar_adapter_cd package header
+ ************************************************************/
+
+#ifndef _IN_
+#define _IN_
+#endif
+#ifndef _OUT_
+#define _OUT_
+#endif
+#ifndef _INOUT_
+#define _INOUT_
+#endif
+
+/* Package dependency header include                        */
+
+namespace pf_radar_adapter_cd {
+
+// Types defined within the package
+}// of namespace pf_radar_adapter_cd
+
+/************************************************************
+ End of Pkg_pf_radar_adapter_cd package header
+ ************************************************************/
+
+#endif
---
+++ pf_radar_adapter/src-gen/pf_radar_adapter_cd/globalenums/Pkg_globalenums.h
@@ -0,0 +1,41 @@
+// --------------------------------------------------------
+// Copyright (c)
+//
+// contributions by author
+//                  author@somewhere.net
+// maintained by    maintainer
+//                  maintainer@somewhere.net
+
+#ifndef PKG_PF_RADAR_ADAPTER_CD_GLOBALENUMS
+#define PKG_PF_RADAR_ADAPTER_CD_GLOBALENUMS
+
+/************************************************************
+ Pkg_globalenums package header
+ ************************************************************/
+
+#include "pf_radar_adapter_cd/Pkg_pf_radar_adapter_cd.h"
+
+#ifndef _IN_
+#define _IN_
+#endif
+#ifndef _OUT_
+#define _OUT_
+#endif
+#ifndef _INOUT_
+#define _INOUT_
+#endif
+
+/* Package dependency header include                        */
+
+namespace pf_radar_adapter_cd {
+namespace globalenums {
+
+// Types defined within the package
+}// of namespace globalenums
+} // of namespace pf_radar_adapter_cd
+
+/************************************************************
+ End of Pkg_globalenums package header
+ ************************************************************/
+
+#endif
